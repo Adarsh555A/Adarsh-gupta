4d2e90ea6b4a5efc72a9a04af4affa497541caca
Adarsh gupta <guptaraghav887@gmail.com>
2024-06-17 22:13:29 +0530

    hi-tech

---
+++ hi-tech/script.js
@@ -0,0 +1,10 @@
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove",(e)=>{
+let x = e.pageX;
+let y = e.pageY;
+
+cursor.style.top = y + "px";
+cursor.style.left = x + "px";
+
+
+})
