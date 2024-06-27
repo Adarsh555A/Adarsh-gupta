b3300a7ab8e8746d2c2404891f7f47967d07dafe
Adarsh gupta <guptaraghav887@gmail.com>
2024-06-27 07:02:30 +0530

    hi

--- hi-tech/script.js
+++ hi-tech/script.js
@@ -18,7 +18,9 @@ cursor.style.left = x + "px";
 
 
 
-
+// document.querySelector('.course').addEventListener("click",()=>{
+//     document.querySelector(".show").classList.add("hide");
+// })
 
 
 
@@ -55,3 +57,89 @@ const typeEffect = () => {
 }
 
 typeEffect();
+
+
+
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+document.querySelectorAll('.course')[0].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[1].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[2].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[3].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[4].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[5].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[6].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[7].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[8].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[9].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+document.querySelectorAll('.course')[10].addEventListener("click",(el)=>{
+    // document.querySelector(".show").classList.add("hide");
+    modal.style.display = "block";
+})
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
