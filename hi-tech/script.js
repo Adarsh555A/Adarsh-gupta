885102f99d20739211a417a53f8f205841bd8b75
Adarsh gupta <106327853+Adarsh555A@users.noreply.github.com>
2024-07-04 21:25:53 +0530

    hite script.js

--- hi-tech/script.js
+++ hi-tech/script.js
@@ -158,3 +158,32 @@ let url = "https://script.google.com/macros/s/AKfycbzm8z_wFLRKXR-YTtlIHMZe8JV5yc
      })    
     e.preventDefault();
     })
+
+
+
+    let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
